be079e1f5ecb6c8491658654c2e804e1fd33ba69
Marc Jansen <jansen@terrestris.de>
2016-09-15 13:56:11 +0200

    Initial commit

---
+++ spec/geo-editor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GeoEditorView from '../lib/geo-editor-view';
+
+describe('GeoEditorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
